e009da32b6805f35dae5eee5d58f0995f63fe7e0
anshulchahar <rohitchahar0987@gmail.com>
2026-02-06 04:40:26 +0100

    feat: add Footer component to enhance layout with copyright information

---
+++ nextjs-temp/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="fixed bottom-0 left-0 w-full py-2 text-center text-xs text-gray-400 dark:text-gray-500 bg-gray-50 dark:bg-[#1E1E1E] border-t border-gray-200 dark:border-gray-700 z-40">
+            Developed by Anshul Chahar
+        </footer>
+    );
+}
